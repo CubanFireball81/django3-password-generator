202e7d3889bc5de1a23e4c96680a3eca11623efc
Alejandro Santiago <cubanfireball81@gmail.com>
2022-05-04 15:20:17 -0800

    Added whitenoise middleware for Heroku deployment

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'password_generator.urls'
